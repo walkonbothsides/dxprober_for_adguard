1bd4a9949bf904419ead437d6ba9c534c575d9e0
walkonbothsides <278871519@qq.com>
2023-06-30 14:10:56 +0800

    Update dxprober_for_adguard.user.js
    弹窗改为状态框

--- dxprober_for_adguard.user.js
+++ dxprober_for_adguard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         舞萌 DX 成绩上传
 // @namespace    https://maimai.wahlap.com
-// @version      0.13
+// @version      0.14
 // @description  dxprober for AdGuard or any other scripts manager
 // @match        https://maimai.wahlap.com/maimai-mobile/*
 // @grant        none
@@ -24,11 +24,30 @@
     uploadButton.style.cursor = 'pointer';
     uploadButton.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.2)';
 
-    // 将按钮添加到网页中
+    // 创建状态框
+    var statusBox = document.createElement('div');
+    statusBox.style.position = 'fixed';
+    statusBox.style.top = '50%';
+    statusBox.style.left = '50%';
+    statusBox.style.transform = 'translate(-50%, -50%)';
+    statusBox.style.padding = '20px';
+    statusBox.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    statusBox.style.color = 'white';
+    statusBox.style.borderRadius = '5px';
+    statusBox.style.fontFamily = 'Arial, sans-serif';
+    statusBox.style.fontSize = '16px';
+    statusBox.style.display = 'none'; // 初始隐藏状态框
+
+    // 将按钮和状态框添加到网页中
     document.body.appendChild(uploadButton);
+    document.body.appendChild(statusBox);
 
     // 监听上传成绩按钮的点击事件
     uploadButton.addEventListener('click', function () {
+        // 显示状态框
+        statusBox.style.display = 'block';
+        statusBox.textContent = '成绩上传中...';
+
         $.ajax({
             url: 'https://maimai.wahlap.com/maimai-mobile/record/musicGenre/search/?genre=99&diff=0',
             type: 'GET',
@@ -39,6 +58,7 @@
                 ((u, p) => {
                     var count = 0;
                     var total = 5;
+                    
                     [0, 1, 2, 3, 4].forEach((diff) => {
                         $.ajax({
                             url: 'https://maimai.wahlap.com/maimai-mobile/record/musicGenre/search/?genre=99&diff=' + diff,
@@ -54,16 +74,24 @@
                                     success: (res) => {
                                         console.log(res);
                                         count++;
+                                        statusBox.textContent = count;
                                         if (count === total) {
-                                            alert('脚本执行完毕！'); // 显示弹窗提示
+                                            statusBox.textContent = '上传完毕！';
+                                            uploadButton.textContent = '成绩已上传';
+                                            // 五秒后隐藏状态框
+                                            setTimeout(function() {
+                                                statusBox.style.display = 'none';
+                                            }, 5000);
+
                                         }
                                     }
                                 });
                             }
                         });
                     });
-                })("USERNAME", "PASSWORD"); // 替换为你的用户名和密码 
+                })("USERNAME", "PASSWORD"); // 替换成自己的用户名和密码
             }
         });
     });
 })();
+    
